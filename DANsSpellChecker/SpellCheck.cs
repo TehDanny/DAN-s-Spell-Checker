a48cb865cb01ea125d157a7dd8e0fe34d5a3a1f6
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-03-08 14:56:08 +0100

    First commit with WPF and unit test

---
+++ DANsSpellChecker/SpellCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DANsSpellChecker
+{
+    public class SpellCheck
+    {
+        public string Word(string writtenWord)
+        {
+            string spellCheckedWord = "";
+            return spellCheckedWord;
+        }
+
+        public string[] Text(string writtenWord)
+        {
+            string[] spellCheckedText = new string[] { "" };
+            return spellCheckedText;
+        }
+    }
+}
---
+++ DANsSpellChecker/WordDatabase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DANsSpellChecker
+{
+    public class WordDatabase
+    {
+        public bool FindWord(string writtenWord)
+        {
+            return true;
+        }
+    }
+}
